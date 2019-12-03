29dd02797b7c7f5ced9b0048c852c21bca4fa7e9
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-12-02 23:16:45 -0800

    Day 1 solution

--- 2019/dist/day-1.js
+++ 2019/dist/day-1.js
@@ -1,6 +1,19 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var getInput = function (file) {
-    return [];
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var sumArr = function (arr) { return arr.reduce(function (a, b) { return a + b; }, 0); };
+var getInput = function () {
+    fs_1.default.readFile("input/day-1.txt", "utf8", function (error, data) {
+        var lines = data.split("\n").map(Number);
+        var fuelNeed = lines.map(function (mass) {
+            return Math.floor(mass / 3) - 2;
+        });
+        var fuel = sumArr(fuelNeed);
+        console.log("Total fuel needed: ", fuel);
+    });
+};
+getInput();
 //# sourceMappingURL=day-1.js.map
--- 2019/src/day-1.ts
+++ 2019/src/day-1.ts
@@ -1,5 +1,19 @@
-import fs from 'fs';
+import fs from "fs";
 
-const getInput = (file: object): String[] => {
-  return []
-}
+const sumArr = (arr: number[]): number => arr.reduce((a, b) => a + b, 0);
+
+const getInput = () => {
+  fs.readFile("input/day-1.txt", "utf8", (error, data) => {
+    const lines = data.split("\n").map(Number);
+
+    const fuelNeed = lines.map((mass: number) => {
+      return Math.floor(mass / 3) - 2;
+    });
+
+    const fuel = sumArr(fuelNeed);
+
+    console.log("Total fuel needed: ", fuel);
+  });
+};
+
+getInput();
